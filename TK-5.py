fa7f525ac0e34da31e6482ebf6053f46c173eb67
hnoievets <hnoievets.O.A@nmu.one>
2021-11-14 18:31:44 +0200

    Create function list_sqrt

---
+++ TK-5.py
@@ -0,0 +1,12 @@
+# need to import sqrt from math
+
+def list_sqrt(data_list):
+    sqrt_list = []
+    if len(data_list) > 0:
+        for i in data_list:
+            sqrt_list += sqrt(i)
+
+        return sqrt_list
+
+    else:
+        return 0
