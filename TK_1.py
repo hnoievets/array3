b07688fb1a683e70ed90f386f55654e77f9e4c83
hnoievets <hnoievets.O.A@nmu.one>
2021-11-14 17:15:54 +0200

    Create function num_in_list.

---
+++ TK_1.py
@@ -0,0 +1,10 @@
+def num_in_list(count):
+    data = []
+    if count > 0:
+        print('Enter ' + str(count) + ' numbers')
+        for i in range(count):
+            data += [int(input(('Enter number ' + str(i+1) + ': ')))]
+    else:
+        return 0
+
+    return data
